c2b7d6a9768d03134004188e5604b498cb35fa62
inffy <inffy@users.noreply.github.com>
2025-06-24 14:04:10 +0300

    install bazaar from copr

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,6 +9,15 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+dnf5 remove -y \
+  default-editor \
+  nano \
+  nano-default-editor \
+  vim-minimal \
+  vim-enhanced \
+  vim-data \
+  ptyxis
+
 # this installs a package from fedora repos
 dnf5 install -y tmux
 dnf5 -y install micro
@@ -17,8 +26,16 @@ dnf5 -y install micro
 #
 # dnf5 -y copr enable ublue-os/staging
 # dnf5 -y install package
+
+dnf5 -y copr enable ublue-os/packages
+dnf5 -y copr enable ublue-os/staging
+
+dnf5 -y install bazaar
+
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable ublue-os/packages
+dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
 
