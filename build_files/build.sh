aa9880da644b785b3ad5f0b1556acee374d68e76
inffy <inffy@users.noreply.github.com>
2025-06-24 13:39:03 +0300

    install micro

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,8 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
-dnf5 install micro
+dnf5 install -y tmux
+dnf5 -y install micro
 
 # Use a COPR Example:
 #
